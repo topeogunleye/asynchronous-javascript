8d03befd6ff309e1c41d72c446e7a5ba70c24095
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-19 09:02:16 +0100

    Add a getTodos function

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,14 @@
-const request = new XMLHttpRequest();
+const getTodos = () => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  } else if (request.readyState === 4) {
-    console.log('could not fetch data');
-  }
-});
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.responseText);
+    } else if (request.readyState === 4) {
+      console.log('could not fetch data');
+    }
+  });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-request.send();
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+  request.send();
+};
