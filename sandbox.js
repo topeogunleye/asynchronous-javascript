969489d5cbf3e6e524b794f9a5a84320e5dc6446
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-19 09:21:00 +0100

    Call reuable todo function

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,22 @@
-const getTodos = () => {
+const getTodos = (callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener('readystatechange', () => {
     if (request.readyState === 4 && request.status === 200) {
-      console.log(request.responseText);
+      callback(undefined, JSON.parse(request.responseText));
     } else if (request.readyState === 4) {
-      console.log('could not fetch data');
+      callback('could not fetch data', undefined);
     }
   });
 
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todose');
   request.send();
 };
+
+getTodos((err, data) => {
+  if (err) {
+    console.log(err);
+  }  else {
+    console.log(data);
+  }
+});
